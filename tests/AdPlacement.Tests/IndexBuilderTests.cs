8fba6d25038ded2643161f1d39b4f7047ddb1a01
Emre <Emre@DESKTOP-8FMJR1I>
2025-08-31 09:51:08 +0300

    [test]Xunit

---
+++ tests/AdPlacement.Tests/IndexBuilderTests.cs
@@ -0,0 +1,29 @@
+﻿
+using AdReplacement.Domain.Models;
+using FluentAssertions;
+
+namespace AdPlacement.Tests;
+
+public class IndexBuilderTests
+{
+    [Fact]
+    public void BuildEffective_Should_Combine_Parent_And_Own()
+    {
+        var b = new IndexBuilder();
+        var y = b.GetOrAddPlatformId("Yandex");
+        var k = b.GetOrAddPlatformId("CoolAds");
+
+
+        b.AddOwn(y, Array.Empty<string>()); // "/"
+        b.AddOwn(k, new[] { "ru", "svrd" });
+
+
+        var idx = b.BuildEffective();
+        var root = idx.Root;
+        root.EffectivePlatformIds.Should().HaveCount(1);
+
+
+        var node = root.Children["ru"].Children["svrd"];
+        idx.MapIdsToNames(node.EffectivePlatformIds).Should().Contain(new[] { "CoolAds", "Yandex" });
+    }
+}
---
+++ tests/AdPlacement.Tests/QueryTests.cs
@@ -0,0 +1,28 @@
+﻿using AdPlacement.Infrastructure.Index;
+using AdPlacement.Infrastructure.Index.Abstactions;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+
+namespace AdPlacement.Tests;
+
+public class QueryTests
+{
+    [Fact]
+    public async Task Query_Returns_Deepest_Match()
+    {
+        var accessor = new PlacementIndexAccessor();
+        var loader = new InMemoryPlacementsLoader(accessor, new LoggerFactory().CreateLogger<InMemoryPlacementsLoader>());
+
+
+        var text = "Yandex:/ru\nLocal:/ru/svrd/revda\nCool:/ru/svrd\n";
+        await loader.ReloadAsync(text);
+
+
+        var query = new InMemoryPlacementQuery(accessor);
+        var result = await query.GetPlatformsAsync("/ru/svrd/revda");
+
+
+        result.Platforms.Should().Contain(new[] { "Cool", "Local", "Yandex" });
+        result.MatchedDepth.Should().Be(3);
+    }
+}
